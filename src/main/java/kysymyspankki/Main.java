108e88a6d5c16e3c980f171ad4c4ab99c28681de
Korhonen Keijo <korhokei@lx7-fuxi198.ad.helsinki.fi>
2018-10-12 11:12:42 +0300

    connect to database

--- src/main/java/kysymyspankki/Main.java
+++ src/main/java/kysymyspankki/Main.java
@@ -1,6 +1,8 @@
 package kysymyspankki;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.Request;
@@ -10,12 +12,22 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, Exception {
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
         
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:kysymyspankki.db");
+        Connection connection = getConnection();
+        
+        PreparedStatement statement = connection.prepareStatement("SELECT kurssi FROM Kysymys");
+        ResultSet result = statement.executeQuery();
+        
+        List<String> kurssit = new ArrayList<>();
+        
+        while (result.next()) {
+            String kurssi = result.getString("kurssi");
+            kurssit.add(kurssi);
+        }
 
         Spark.get("*", (req, res) -> {
             HashMap map = new HashMap<>();
@@ -23,6 +35,16 @@ public class Main {
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
+        
+        connection.close();
     }
 
+    public static Connection getConnection() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+        
+        return DriverManager.getConnection("jdbc:sqlite:kysymyspankki.db");
+    }
 }
