586cd44e1c4e42f300d24fc5fabd46357cd7396e
Korhonen Keijo <korhokei@lx7-fuxi198.ad.helsinki.fi>
2018-10-19 11:27:28 +0300

    Remove all answers for a questions when question is removed

--- src/main/java/kysymyspankki/Main.java
+++ src/main/java/kysymyspankki/Main.java
@@ -3,6 +3,7 @@ package kysymyspankki;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import kysymyspankki.dao.KysymysDao;
 import kysymyspankki.dao.VastausDao;
 import kysymyspankki.database.Database;
@@ -15,28 +16,28 @@ import spark.Spark;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
-
+    
     public static void main(String[] args) throws SQLException, Exception {
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
-
+        
         Database database = new Database("jdbc:sqlite:kysymyspankki.db");
         KysymysDao kysymysDao = new KysymysDao(database);
         VastausDao vastausDao = new VastausDao(database, kysymysDao);
-
+        
         Spark.get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("kysymykset", kysymysDao.findAll());
-
+            
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-
+        
         Spark.post("/", (req, res) -> {
             String kurssi = req.queryParams("kurssi");
             String aihe = req.queryParams("aihe");
             String kysymysteksti = req.queryParams("kysymysteksti");
-
+            
             if (!kurssi.isEmpty() && !aihe.isEmpty() && !kysymysteksti.isEmpty()
                     && kurssi.length() <= 50 && aihe.length() <= 50 && kysymysteksti.length() <= 255) {
                 kysymysDao.saveOrUpdate(new Kysymys(null, kurssi, aihe, kysymysteksti));
@@ -44,48 +45,54 @@ public class Main {
             res.redirect("/");
             return "";
         });
-
+        
         Spark.get("/kysymykset/:id", (req, res) -> {
             HashMap map = new HashMap<>();
             int kysymysId = Integer.parseInt(req.params("id"));
-
+            
             map.put("kysymys", kysymysDao.findOne(kysymysId));
             map.put("vastaukset", vastausDao.findAllByKysymysId(kysymysId));
             return new ModelAndView(map, "kysymys");
         }, new ThymeleafTemplateEngine());
-
+        
         Spark.post("/kysymykset/:id", (req, res) -> {
             String vastausteksti = req.queryParams("vastausteksti");
             String checkboxValue = req.queryParams("oikein");
-
+            
             int kysymysId = Integer.parseInt(req.params("id"));
-
+            
             Boolean oikein = null;
-
+            
             if (checkboxValue == null) {
                 oikein = false;
             } else {
                 oikein = true;
             }
-
+            
             Kysymys kysymys = kysymysDao.findOne(Integer.parseInt(req.params("id")));
-
+            
             if (!vastausteksti.isEmpty() && vastausteksti.length() <= 255) {
                 vastausDao.saveOrUpdate(new Vastaus(null, kysymys, vastausteksti, oikein));
             }
             res.redirect("/kysymykset/" + kysymysId);
             return "";
         });
-
+        
         Spark.post("/poista/:id", (req, res) -> {
-            kysymysDao.delete(Integer.parseInt(req.params("id")));
+            int kysymysId = Integer.parseInt(req.params("id"));
+            
+            List<Vastaus> vastaukset = vastausDao.findAllByKysymysId(kysymysId);
+            for (Vastaus vastaus : vastaukset) {
+                vastausDao.delete(vastaus.getId());
+            }
+            kysymysDao.delete(kysymysId);
             res.redirect("/");
             return "";
         });
-
+        
         Spark.post("/kysymykset/:kysymysid/poista/:id", (req, res) -> {
             int kysymysId = Integer.parseInt(req.params("kysymysid"));
-
+            
             vastausDao.delete(Integer.parseInt(req.params("id")));
             res.redirect("/kysymykset/" + kysymysId);
             return "";
